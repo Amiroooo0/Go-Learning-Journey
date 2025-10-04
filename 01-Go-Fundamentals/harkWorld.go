56bde23f0843621a4b842121eae0f8efd7c40ebf
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-10-04 16:00:23 +0330

    feat(fundamentals): Initial setup and basic 'Hello World' example.

---
+++ 01-Go-Fundamentals/harkWorld.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+func main(){
+	hark()
+}
+func hark() {
+	fmt.Printf("Hark, World! Amir the Sworn hath come to deliver the realm from the Age of Dark, and to kindle the flame of the First Furnace.")
+}
