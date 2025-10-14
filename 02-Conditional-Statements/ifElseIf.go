602502e226a83ab3f6f7eed7164d95afe79840db
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-10-14 10:53:23 +0330

    refactor(fundamentals): edit name of fundamentals section

---
+++ 02-Conditional-Statements/ifElseIf.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	const minUserSalary float64 = 5_600_000
+	var UserSalary float64
+	var taxPercent float64 = 0
+
+	fmt.Printf("Enter your Salary :")
+	fmt.Scanf("%f",&UserSalary)
+
+	if UserSalary <= minUserSalary {
+		taxPercent = 0
+	} else if UserSalary > minUserSalary && UserSalary <= minUserSalary*2 {
+		taxPercent = 0.05
+	} else if UserSalary > minUserSalary*2 && UserSalary <= minUserSalary*3 {
+		taxPercent = 0.07
+	} else if UserSalary > minUserSalary*3 && UserSalary <= minUserSalary*4 {
+		taxPercent = 0.10
+	} else {
+		taxPercent = 0.15
+	}
+
+	fmt.Printf("Your tax percent is: %.2f\n", taxPercent)
+	fmt.Printf("Your tax is: %.2f\n", taxPercent*UserSalary)
+	fmt.Printf("Your UserSalary is: %.2f\n", UserSalary-taxPercent*UserSalary)
+}
