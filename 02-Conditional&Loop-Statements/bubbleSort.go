c5644c7dec2f93b5e4939ef99574b608930c1f35
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-10-19 01:50:02 +0330

    feat(loops): optimization of the Babel Sort algorithm using the issorted and swap auxiliary functions

--- 02-Conditional&Loop-Statements/bubbleSort.go
+++ 02-Conditional&Loop-Statements/bubbleSort.go
@@ -3,62 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	lst := []int{96, 4321, -85, -85, 78, -6, 0}
+	lst := []int{66, -6, -6, 2, 45623, 0, 21, -75, 50, 8}
 	BubbleSort(lst)
 	fmt.Printf("%v", lst)
 }
 
 func BubbleSort(list []int) {
-	finished := false
-	index := 0
-	for !finished {
-		for i, item := range list {
-			if item > list[i+1] {
-				temp := i
-				i = list[i+1]
-				list[i+1] = temp
-				for j := i + 1; j < len(list)-1; j++ {
-					temp := i
-					i = list[i+1]
-					list[i+1] = temp
+	for i := 0; i < len(list)-1; i++ {
+		if !IsSorted(list) {
+			for j := 0; j < len(list)-1-i; j++ {
+				if list[j] > list[j+1] {
+					Swap(&list[j], &list[j+1])
 				}
 			}
-			index = i
-		}
-		if index == len(list)-1 {
-			finished = true
-		} else {
-			finished = false
 		}
 	}
 }
 
-/*
-96 , 4321 , -85 , -85 , 78 , -6 , 0		1  step
+func IsSorted(list []int) bool {
+	for i := 0; i < len(list)-1; i++ {
+		if list[i] > list[i+1] {
+			return false
+		}
+	}
+	return true
+}
 
-*96 , 4321 , -85 , -85 , 78 , -6 , 0	2  step
-
-96 ,-85 , -85 , 78 , -6 , 0 , *4321		3  step
-
--85 , -85 , 78 , -6 , 0 , *96 , 4321	4  step
-
-*-85 , -85 , 78 , -6 , 0 , 96 , 4321	5  step
-
--85 , *-85 , 78 , -6 , 0 , 96 , 4321	6  step
-
--85 , -85 , -6 , 0 , *78 , 96 , 4321	7  step
-
-*-85 , -85 , -6 , 0 , 78 , 96 , 4321	8  step
-
--85 , *-85 , -6 , 0 , 78 , 96 , 4321	9  step
-
--85 , -85 , *-6 , 0 , 78 , 96 , 4321	10 step
-
--85 , -85 , -6 , *0 , 78 , 96 , 4321	11 step
-
--85 , -85 , -6 , 0 , *78 , 96 , 4321	12 step
-
--85 , -85 , -6 , 0 , 78 , *96 , 4321	13 step
-
--85 , -85 , -6 , 0 , 78 , 96 , *4321	14 step
-*/
+func Swap(a *int, b *int) {
+	*a, *b = *b, *a
+}
