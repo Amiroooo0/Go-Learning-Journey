3d8e6735c81ab30e137dcc87eab1872d50f24ccf
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-12-05 23:30:04 +0330

    feat(functions): an hotel examole for multiple returns

---
+++ 04-Functions/01-Intro/multipleReturn.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	order, tax := CalculateRoomPrice("standard", 3, 2)
+
+	fmt.Printf("order price: %d, tax: %f\n", order, tax)
+}
+
+func CalculateRoomPrice(roomType string, nights int, personCount int) (int, float64) {
+	var price int
+	var tax float64
+
+	switch roomType {
+	case "standard":
+		price = nights * 140000 * personCount
+	case "double":
+		price = nights * 220000 * personCount
+	case "suite":
+		price = nights * 350000 * personCount
+	}
+
+	tax = float64(price) * 0.09
+	return price, tax
+}
